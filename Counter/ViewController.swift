e1c093f0a69ec57e1238d2f04d9b098b5679d0b5
Andrey Sopov <lechuk@yandex.ru>
2024-12-01 15:48:18 +0300

    Окончательный вариант сложной задачи

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,42 +22,48 @@ class ViewController: UIViewController {
         }
         
     }
-    
+    @IBOutlet weak var historyTextView: UITextView! { didSet {
+        historyTextView.text = "История изменений :"
+    }
+        
+    }
     @IBOutlet weak var counterLabel: UILabel! {
         didSet {
             counterLabel.text = "Значение счётчика:\(count)"
         }
     }
     
-        
+    
     @IBAction func addCount(_ sender: Any) {
         
         count += 1
-        setLabel(sender,count)
-   
+        setLabel(sender,count, "значение изменено на +1")
+        
     }
     
     @IBAction func removeCount(_ sender: Any) {
         
         if count > 0 {
             count -= 1
+            setLabel(sender,count,"значение изменено на -1")
         }
         else {
             count = 0
+            setLabel(sender,count,"попытка уменьшить значение счётчика ниже 0")
         }
-        setLabel(sender,count)
+        
     }
     @IBAction func resetCount(_ sender: Any) {
         
         count = 0
-        setLabel(sender,count)
+        setLabel(sender,count, "значение сброшено")
     }
     
-    func setLabel (_ sender: Any,_ count: Int) {
-            
+    func setLabel (_ sender: Any,_ count: Int, _ dataCount: String) {
         
-            counterLabel.text = "Значение счётчика:\(count)"
         
+        counterLabel.text = "Значение счётчика:\(count)"
+        historyTextView.text += "\n\(dataCount) \(Date.now.formatted(date: .numeric, time: .shortened))"
         
     }
     
